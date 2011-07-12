5d611bc13894a72157e04826bfd0b0fb33cd03c0
Vincent Bernat <bernat@luffy.cx>
2011-07-12 22:18:54 +0200

    color: black and white manual pages

--- rc/colors.zsh
+++ rc/colors.zsh
@@ -8,12 +8,4 @@
     # grep colors
     export GREP_OPTIONS='--color=auto'
     export GREP_COLOR='1;32'
-
-    # less colors
-    export LESS_TERMCAP_md=$fg_bold[green]
-    export LESS_TERMCAP_us=$fg_no_bold[yellow]
-    export LESS_TERMCAP_so=$bg[magenta]$fg_bold[white]
-    export LESS_TERMCAP_me=$reset_color
-    export LESS_TERMCAP_se=$reset_color
-    export LESS_TERMCAP_ue=$reset_color
 }
