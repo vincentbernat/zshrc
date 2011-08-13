171755dec117f1f484717d65bc577d3ad8efbfa0
Vincent Bernat <bernat@luffy.cx>
2011-08-13 12:38:03 +0200

    prompt: include schroot name in prompt if available

---
+++ rc/chroot.zsh
@@ -0,0 +1,9 @@
+# -*- sh -*-
+
+# Include schroot name in prompt if available
+[[ -z SCHROOT_CHROOT_NAME ]] || {
+    _vbe_add_prompt_schroot () {
+	print -n '${PR_BLUE}(${PR_YELLOW}sch:${PR_NO_COLOUR}${SCHROOT_CHROOT_NAME}${PR_BLUE})'
+	print -n '$PR_CYAN$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'
+    }
+}
