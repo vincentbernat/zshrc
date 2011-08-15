d7fcf475f591b7bcd1d31cc70de804bdf260907a
Vincent Bernat <bernat@luffy.cx>
2011-08-15 18:46:12 +0200

    tmp: keep ~/tmp in the home directory, not as a link to /tmp

--- rc/00-temp.zsh
+++ rc/00-temp.zsh
@@ -1,7 +1,12 @@
 # -*- sh -*-
 
 # Safe creation of temporary directory
-[ -h ~/tmp ] && [ ! -d ~/tmp ] && {
+([ -h ~/tmp ] || [ ! -d ~/tmp ]) && {
     rm ~/tmp 2> /dev/null
-    ln -s $(mktemp -d) ~/tmp
+    mkdir ~/tmp
+    # ln -s $(mktemp -d) ~/tmp
 }
+[ -d ~/tmp ] && {
+    rm ~/tmp/**/*(U.mw+3)
+    rmdir ~/tmp/**/*(U/mw+3)
+}  2> /dev/null
