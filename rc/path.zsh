ec28eddb5ec957e4aa200ea828d6c350440e4264
Vincent Bernat <bernat@luffy.cx>
2011-08-25 13:35:00 +0200

    path: add /usr/lib/ccache to path.

--- rc/path.zsh
+++ rc/path.zsh
@@ -2,9 +2,11 @@
 
 __() {
     local wanted
-    wanted=(/usr/local/sbin /usr/sbin /sbin /usr/local/bin /usr/bin /bin)
+    local p
+    wanted=(/usr/local/sbin /usr/sbin /sbin /usr/local/bin /usr/bin /bin /usr/lib/ccache ~/bin)
     for p in $wanted; do
-	(( ${${path[(r)$p]}:+1} )) || \
-	    path=($path $p)
+	(( ${${path[(r)$p]}:+1} )) || {
+	    [ -d $p ] && path=($p $path)
+	}
     done
 } && __
