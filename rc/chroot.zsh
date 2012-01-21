2390b149e1630d028fda3e19ce450338b86a3382
Vincent Bernat <bernat@luffy.cx>
2012-01-21 22:56:53 +0100

    Include chroot name when inside dchroot

--- rc/chroot.zsh
+++ rc/chroot.zsh
@@ -1,6 +1,8 @@
 # -*- sh -*-
 
 # Include schroot name in prompt if available
+[[ ! -f /etc/debian_chroot ]] || \
+    SCHROOT_CHROOT_NAME=$(</etc/debian_chroot)
 [[ -z $SCHROOT_CHROOT_NAME ]] || {
     _vbe_add_prompt_schroot () {
 	print -n '${PR_BLUE}(${PR_YELLOW}sch:${PR_NO_COLOUR}${SCHROOT_CHROOT_NAME}${PR_BLUE})'
