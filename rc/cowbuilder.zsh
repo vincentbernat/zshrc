438cb7dbc408acf8ffbdf5b02cbf352dbfdefcf7
Vincent Bernat <bernat@luffy.cx>
2012-04-13 23:45:30 +0200

    cowbuilder: use a function with only one parameter

--- rc/cowbuilder.zsh
+++ rc/cowbuilder.zsh
@@ -1,15 +1,30 @@
 # -*- sh -*-
 
-# Cowbuilder + multistrap
 (( $+commands[cowbuilder] )) && {
-    _vbe_cowbuilder() {
-	# Distribution. Something like debian/squeeze
-        local distrib=$1
-	# Architecture (optional)
-	local arch=${2:-$(dpkg --print-architecture)}
-	shift 2
+    cowbuilder() {
+	# First argument is something like this:
+	#   debian/squeeze
+	#   debian/squeeze/i386
+	#   ubuntu/oneiric/amd64
+	#   ubuntu/oneiric.custom
+	# Remaining arguments are those for cowbuilder
 
+	# Architecture
+	local arch
+	case $1 in
+	    */*/*)
+		arch=${1##*/}
+		distrib=${1%/*}
+		;;
+	    *)
+		arch=$(dpkg --print-architecture)
+		distrib=$1
+		;;
+	esac
+	shift
         local opts="--debootstrapopts --arch --debootstrapopts $arch"
+
+	# Distribution
 	case $distrib in
 	    debian/*)
 		opts="$opts --mirror http://ftp.fr.debian.org/debian"
@@ -20,18 +35,15 @@
 		opts="$opts --debootstrapopts --keyring --debootstrapopts /usr/share/keyrings/ubuntu-archive-keyring.gpg"
 		;;
 	esac
+	distrib=${distrib##*/}
+
         local target=$distrib.$arch
-	_vbe_title "cowbuilder-$distrib: $@"
+	distrib=${distrib%.*}
+	_vbe_title "cowbuilder $target: $@"
         sudo env DEBIAN_BUILDARCH="$arch" cowbuilder "$@" \
-	    --distribution ${distrib##*/}  \
+	    --distribution ${distrib}  \
             --basepath /var/cache/pbuilder/bases/$target.cow \
             --buildresult /var/cache/pbuilder/results/$target \
             ${=opts}
     }
-    alias cowbuilder-sid='_vbe_cowbuilder debian/sid ""'
-    alias cowbuilder-squeeze='_vbe_cowbuilder debian/squeeze ""'
-    alias cowbuilder-lenny='_vbe_cowbuilder debian/lenny ""'
-    alias cowbuilder-maverick-i386='_vbe_cowbuilder ubuntu/maverick i386'
-    alias cowbuilder-oneiric-i386='_vbe_cowbuilder ubuntu/oneiric i386'
-    alias cowbuilder-precise-i386='_vbe_cowbuilder ubuntu/precise i386'
 }
