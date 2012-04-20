eae17159b08cd8c54478e28939454c5821c5333b
Vincent Bernat <bernat@luffy.cx>
2012-04-20 12:45:12 +0200

    Add proper LDAP completion.
    LDAPHOST needs to be defined (in ~/.zshrc for example).

--- plugins/hebex.plugin.zsh
+++ plugins/hebex.plugin.zsh
@@ -1,19 +1,5 @@
 # -*- sh -*-
 
-# Grab some hosts from LDAP
-if [[ $HOSTNAME == *.fti.net ]] && [[ -n $SSH_AUTH_SOCK ]]; then
-    zmodload zsh/stat
-    zmodload zsh/datetime
-    if [[ ! -s $ZSH/local/hosts.hebex ]] || \
-	[[ $(($EPOCHSECONDS - $(stat +mtime $ZSH/local/hosts.hebex))) -gt 86400 ]]; then
-	echo -n "Rebuilding host cache... "
-	ssh ldap01.infra.b2.p.fti.net "slapcat -s ou=hosts,dc=fti,dc=net | grep ^dn:" | \
-	    sed -n 's/^dn: cn=\([^,]*\),.*/\1/p' | \
-	    grep -E '\.(net|infra)\.'> $ZSH/local/hosts.hebex
-	echo "done."
-    fi
-fi
-
 # Telnet through HNM
 telnet() {
     _vbe_title "$@"
--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -23,12 +23,28 @@ zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH/run/cache/
 zstyle ':completion:*:descriptions' format '%B%d%b'
 
-zstyle -e ':completion:*' hosts 'reply=(
-        # Take hosts from /etc/hosts
-	${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}
-	# And from local/hosts.*
-	$(cat $ZSH/local/hosts.*(|2)(N) /dev/null)
-    )'
+# Host completion
+_custom_hosts() {
+    # Complete ~/.zsh/local/hosts.*
+    local host
+    for host in $ZSH/local/hosts.*(N); do
+	_wanted hosts expl host compadd "$@" $(<$host)
+    done
+
+    # And /etc/hosts
+    _wanted hosts expl host \
+	compadd "$@" ${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}
+
+    # Now, try LDAP
+    [[ -z $LDAPHOST ]] || {
+	_wanted hosts expl host \
+	    compadd "$@" ${$(ldapsearch -h $LDAPHOST \
+	    -b "ou=hosts,dc=fti,dc=net" -LLL -s sub -z 100 \
+	    -x "cn=${words[CURRENT]}*" cn 2> /dev/null)%* }
+    }
+}
+
+zstyle -e ':completion:*' hosts _custom_hosts
 
 # In menu, select items with +
 zmodload -i zsh/complist
