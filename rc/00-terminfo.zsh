688c20e48c2df418d2b833f0cca9139619f37106
Vincent Bernat <bernat@luffy.cx>
2012-06-12 12:22:26 +0200

    terminfo: rework how we compute terminfo files.

--- rc/00-terminfo.zsh
+++ rc/00-terminfo.zsh
@@ -1,7 +1,17 @@
 # -*- sh -*-
 
-[[ $ZSH/rxvt-unicode.terminfo -nt $ZSH/rxvt-unicode.terminfo ]] && \
-    TERMINFO=~/.terminfo tic $ZSH/rxvt-unicode.terminfo
+# Update terminfo
+__() {
+    local terminfo
+    local termpath
+    for terminfo in $ZSH/terminfo/*.terminfo(.N); do
+	# We assume that the file is named appropriately for this to work
+	termpath=~/.terminfo/${(@)${terminfo##*/}[1]}/${${terminfo##*/}%%.terminfo}
+	if [[ ! -e $termpath ]] || [[ $terminfo -nt $termpath ]]; then
+	    TERMINFO=~/.terminfo tic $terminfo
+	fi
+    done
+} && __
 
 autoload -U colors zsh/terminfo
 [[ "$terminfo[colors]" -ge 8 ]] && colors
