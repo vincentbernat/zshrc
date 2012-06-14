a7834f78b0c94a378b019f459d2501d422029c09
Vincent Bernat <bernat@luffy.cx>
2012-06-14 16:35:25 +0200

    alias: add `o` to open anything through `gvfs-open`.

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -10,3 +10,6 @@ alias ll='ls -l'
 alias susu='sudo env HISTFILE=$HISTFILE-root HOME="$HOME" zsh'
 alias us='setxkbmap us "" compose:rwin ; xmodmap -e "keysym Pause = XF86ScreenSaver"'
 alias fr='setxkbmap fr'
+
+# Open anything (needs gvfs-open from `gvfs-bin` package)
+(( $+commands[gvfs-open] )) && alias o='gvfs-open'
