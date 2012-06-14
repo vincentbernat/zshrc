f97827651377f22f6f7c093d9d7e6a4a9e24426a
Vincent Bernat <bernat@luffy.cx>
2012-06-14 10:37:59 +0200

    options: enable autopushd and autocd.
    See this for more:
     https://groups.google.com/forum/?fromgroups#!topic/fr.comp.os.unix/jDwtWFPDAZ8

--- rc/options.zsh
+++ rc/options.zsh
@@ -2,7 +2,10 @@
 
 # Use emacs keybindings
 bindkey -e
-# Allow comments inside commands
-setopt interactivecomments
+
+setopt interactivecomments	# Allow comments inside commands
+setopt autopushd		# Maintain directories in a heap
+setopt autocd			# Don't need to use `cd`
+
 # meta-h will invoke man for the current command
 autoload run-help
