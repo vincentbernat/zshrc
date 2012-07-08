7231812639948552b324cafddff738e23e353fd1
Vincent Bernat <bernat@luffy.cx>
2012-07-08 09:41:46 +0200

    alias: remove us/fr aliases
    Keyboard configuration is not really the shell job.

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -8,8 +8,6 @@ alias ll='ls -l'
 
 # Less generic aliases
 alias susu='sudo env HISTFILE=$HISTFILE-root HOME="$HOME" zsh'
-alias us='setxkbmap us "" compose:rwin ; xmodmap -e "keysym Pause = XF86ScreenSaver"'
-alias fr='setxkbmap fr'
 
 # Open anything (needs gvfs-open from `gvfs-bin` package)
 (( $+commands[gvfs-open] )) && alias o='gvfs-open'
