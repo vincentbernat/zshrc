28da7a9548d0a6b18c1f8d92e4b0545798b52192
Vincent Bernat <bernat@luffy.cx>
2012-07-16 10:44:34 +0200

    colors: add a function to display a color test card

--- rc/colors.zsh
+++ rc/colors.zsh
@@ -9,3 +9,24 @@
     export GREP_OPTIONS='--color=auto'
     export GREP_COLOR='1;32'
 }
+
+# From: http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+colortest() {
+    local T='gYw'   # The test text
+
+    local fg
+    local bg
+    printf "\n%12s" ""
+    for fg in {40..47}; do
+	printf "%7sm" ${fg}
+    done
+    printf "\n"
+    for fg in 0 1 $(for i in {30..37}; do echo $i 1\;$i; done); do
+	printf " %5s \e[%s  %s  " ${fg}m ${fg}m ${T}
+	for bg in {40..47}m; do
+	    printf " \e[%s\e[%s  %s  \e[0m" ${fg}m ${bg} ${T}
+	done
+	printf "\n"
+    done
+    printf "\n"
+}
