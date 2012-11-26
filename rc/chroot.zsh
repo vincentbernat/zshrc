b7df0f06618d6e412341b3733df0176f20a54f0f
Vincent Bernat <vincent.bernat@dailymotion.com>
2012-11-26 09:52:02 +0100

    prompt: add an indication when inside pbuilder

--- rc/chroot.zsh
+++ rc/chroot.zsh
@@ -9,3 +9,27 @@
 	print -n '$PR_CYAN$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'
     }
 }
+
+# In pbuilderrc, add:
+#   export PBUILDERPID=$$
+[[ -z $PBUILDERPID ]] || {
+    _vbe_add_prompt_pbuilder () {
+	print -n '${PR_BLUE}(${PR_YELLOW}pb:${PR_NO_COLOUR}${PBUILDERPID}${PR_BLUE})'
+	print -n '$PR_CYAN$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'
+    }
+}
+
+# Here is the whole snippet that I am using:
+
+# if [ "x$PBCURRENTCOMMANDLINEOPERATION" = xlogin ]; then
+#   export PBUILDERPID=$$
+#   [ -z $SUDO_UID ] || {
+#     home=$(getent passwd $SUDO_UID | awk -F: '{print $6}')
+#     BINDMOUNTS="$BINDMOUNTS $home"
+#     export ZDOTDIR=$home
+#   }
+# fi
+
+# Once inside pbuilder, I just do:
+#  apt-get install zsh
+#  exec zsh
