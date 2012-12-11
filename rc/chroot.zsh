bdab9ff3d870fba6f0f7f999533088bff06ef60e
Vincent Bernat <vincent.bernat@dailymotion.com>
2012-12-11 11:12:24 +0100

    chroot: add a special prompt for LXC

--- rc/chroot.zsh
+++ rc/chroot.zsh
@@ -1,7 +1,24 @@
 # -*- sh -*-
 
+# Are we running inside lxc? lxc sets `container` environment variable
+# for PID 1 but this seems difficult to get as a simple
+# user. Therefore, we will look at /proc/self/cgroup.
+[[ ! -f /proc/self/cgroup ]] || {
+    case $(</proc/self/cgroup) in
+        *:/lxc/*)
+            LXC_CHROOT_NAME=${$(</proc/self/cgroup)##*:/lxc/}
+                ;;
+    esac
+}
+[[ -z $LXC_CHROOT_NAME ]] || {
+    _vbe_add_prompt_lxc () {
+	print -n '${PR_BLUE}(${PR_YELLOW}lxc:${PR_NO_COLOUR}${LXC_CHROOT_NAME}${PR_BLUE})'
+	print -n '$PR_CYAN$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'
+    }
+}
+
 # Include schroot name in prompt if available
-[[ ! -f /etc/debian_chroot ]] || \
+[[ ! -f /etc/debian_chroot ]] || [[ -n $LXC_CHROOT_NAME ]] || \
     SCHROOT_CHROOT_NAME=$(</etc/debian_chroot)
 [[ -z $SCHROOT_CHROOT_NAME ]] || {
     _vbe_add_prompt_schroot () {
