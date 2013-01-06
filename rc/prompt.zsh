b66c2c451cba0ba64279604d5a0717d41615e83d
Vincent Bernat <bernat@luffy.cx>
2013-01-06 12:22:18 +0100

    prompt: disable coloring if no color available or no terminfo support.
    This happens on RHEL 5.

--- rc/prompt.zsh
+++ rc/prompt.zsh
@@ -30,8 +30,13 @@ else
 fi
 
 for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE GREY; do
-    eval PR_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
-    eval PR_LIGHT_$color='%{$fg[${(L)color}]%}'
+    if [[ "$terminfo[colors]" -ge 8 ]]; then
+        eval PR_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
+        eval PR_LIGHT_$color='%{$fg[${(L)color}]%}'
+    else
+        eval PR_$color=""
+        eval PR_LIGHT_$color=""
+    fi
 done
 PR_NO_COLOUR="%{$terminfo[sgr0]%}"
 
