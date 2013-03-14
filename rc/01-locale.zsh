9432c734b7e8d12b1b777675f8f4bb183fa25a4e
Vincent Bernat <bernat@luffy.cx>
2013-03-14 08:05:34 +0100

    locale: don't use grep to detect locale, split into an array

--- rc/01-locale.zsh
+++ rc/01-locale.zsh
@@ -1,13 +1,15 @@
 # -*- sh -*-
 
 (( $+commands[locale] )) && __() {
+    local available
     local locales
     local locale
     locales=( "LANG fr_FR.utf8 en_US.utf8 C.UTF-8 C" \
 	      "LC_MESSAGES en_US.utf8 fr_FR.utf8 C.UTF-8 C" )
+    available=("${(f)$(locale -a)}")
     for locale in $locales; do
 	for l in $=locale[(w)2,-1]; do
-	    if locale -a | grep -qx $l; then
+            if (( ${available[(i)$l]} <= ${#available} )); then
 		export $locale[(w)1]=$l
 		break
 	    fi
