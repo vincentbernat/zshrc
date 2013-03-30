7c831d1e0a960a5e39336628a3b777784361c758
Vincent Bernat <bernat@luffy.cx>
2013-03-30 23:05:59 +0100

    completion: with kill, always display the menu
    This is useful since a name get completed to a PID: we want to make
    sure we completed the right name.

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -18,6 +18,7 @@ zstyle ':completion:*' list-prompt ''
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:kill:*' force-list always
 zstyle ':completion:*:*:*:*:processes' command "ps -eo pid,user,comm -w -w"
 zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH/run/cache/
