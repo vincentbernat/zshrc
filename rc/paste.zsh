823d8f5269f98f5911311c4fd6beb8c8984f4f5a
Vincent Bernat <bernat@luffy.cx>
2013-04-09 09:28:39 +0200

    paste: add a snippet to do "safe" paste
    See:
     https://github.com/robbyrussell/oh-my-zsh/pull/1698
     http://thejh.net/misc/website-terminal-copy-paste
     http://www.ush.it/team/ascii/hack-tricks_253C_CCC2008/wysinwyc/what_you_see_is_not_what_you_copy.txt
    
    This is still not safe but useful for accidental copy/paste

---
+++ rc/paste.zsh
@@ -0,0 +1,53 @@
+# -*- sh -*-
+# Code from Mikael Magnusson: http://www.zsh.org/mla/users/2011/msg00367.html
+#
+# Requires xterm, urxvt, iTerm2 or any other terminal that supports bracketed
+# paste mode as documented: http://www.xfree86.org/current/ctlseqs.html
+
+[[  $TERM == rxvt-unicode || \
+    $TERM == xterm || \
+    $TERM == xterm-256color || \
+    $TERM == screen-256color ]] && __() {
+
+    # create a new keymap to use while pasting
+    bindkey -N paste
+    bindkey -R -M paste "^@"-"\M-^?" paste-insert
+    bindkey '^[[200~' _start_paste
+    bindkey -M paste '^[[201~' _end_paste
+    bindkey -M paste -s '^M' '^J'
+
+    zle -N _start_paste
+    zle -N _end_paste
+    zle -N zle-line-init _zle_line_init
+    zle -N zle-line-finish _zle_line_finish
+    zle -N paste-insert _paste_insert
+
+    # switch the active keymap to paste mode
+    function _start_paste() {
+        bindkey -A paste main
+    }
+
+    # go back to our normal keymap, and insert all the pasted text in the
+    # command line. this has the nice effect of making the whole paste be
+    # a single undo/redo event.
+    function _end_paste() {
+        bindkey -e
+        LBUFFER+=$_paste_content
+        unset _paste_content
+    }
+
+    function _paste_insert() {
+        _paste_content+=$KEYS
+    }
+
+    function _zle_line_init() {
+        # Tell terminal to send escape codes around pastes.
+        printf '\e[?2004h'
+    }
+
+    function _zle_line_finish() {
+        # Tell it to stop when we leave zle, so pasting in other programs
+        # doesn't get the ^[[200~ codes around the pasted text.
+        printf '\e[?2004l'
+    }
+} && __
