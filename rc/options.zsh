784205bcaa302895bffc495ae2a8d3a2ffb79a3a
Vincent Bernat <bernat@luffy.cx>
2013-04-13 12:39:35 +0200

    options: enable extending globbing
    This allows one to use `*~*.bak` for example.

--- rc/options.zsh
+++ rc/options.zsh
@@ -7,6 +7,7 @@ setopt rmstarsilent             # Don't ask for confirmation on rm *
 setopt interactivecomments	# Allow comments inside commands
 setopt autopushd		# Maintain directories in a heap
 setopt autocd			# Don't need to use `cd`
+setopt extendedglob             # Enable extended globbing
 
 # meta-h will invoke man for the current command
 autoload run-help
