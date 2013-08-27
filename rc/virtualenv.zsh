88d2e509a999935edc4989e3c27b5a46e4577d34
Vincent Bernat <vincent.bernat@dailymotion.com>
2013-08-27 15:59:28 +0200

    virtualenv: make it compatible with "gem"

--- rc/virtualenv.zsh
+++ rc/virtualenv.zsh
@@ -12,6 +12,8 @@
 #  $ pip install nodeenv
 #  $ nodeenv -p
 
+# Also setup the environment for gems.
+
 (( $+commands[virtualenv] )) && {
     WORKON_HOME=${WORKON_HOME:-~/.virtualenvs}
     [[ -d $WORKON_HOME ]] || mkdir -p $WORKON_HOME
@@ -50,11 +52,20 @@
 	# If in another environment, call deactivate
 	(( $+functions[deactivate] )) && {
 	    deactivate
+            [[ -z $_OLD_GEM_HOME ]] || export $GEM_HOME=$_OLD_GEM_HOME
+            [[ -z $_OLD_GEM_PATH ]] || export $GEM_PATH=$_OLD_GEM_PATH
 	}
 	[[ $env == "-" ]] || {
 	    local VIRTUAL_ENV_DISABLE_PROMPT=1
 	    local NODE_VIRTUAL_ENV_DISABLE_PROMPT=1
 	    source $activate
+
+            # Gems
+            _OLD_GEM_HOME=$GEM_HOME
+            export GEM_HOME=$VIRTUAL_ENV/gems
+            _OLD_GEM_PATH=$GEM_PATH
+            export GEM_PATH=""
+            path=( $GEM_HOME/bin $path )
 	}
 	rehash
     }
