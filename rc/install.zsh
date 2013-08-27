6ccf6b0fc8ed87274d4fa0ec623f8f5c73a27794
Vincent Bernat <bernat@luffy.cx>
2013-08-28 00:15:39 +0200

    install: copy submodules as well

--- rc/install.zsh
+++ rc/install.zsh
@@ -47,7 +47,11 @@ install-zsh() {
 
         # Uncompress the archive
         echo 'cat <<EOA | $BASE64 | gzip -dc | tar -C $ZSH -xf -'
-	(cd $ZSH ; git archive HEAD) | gzip -c | base64
+	(
+            cd $ZSH
+            git ls-files
+            git submodule --quiet foreach --recursive 'git ls-files --with-tree=${sha1} | sed s+^+$path/+'
+        ) | tar -zcf - -T - | base64
         echo 'EOA'
         echo '}'
         echo 'upgrade'
