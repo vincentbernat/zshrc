85adcef1c5dcef6fd20ea6e529554ba667d28f26
Vincent Bernat <vincent.bernat@dailymotion.com>
2013-10-03 14:54:58 +0200

    bookmarks: add `g` to jump to a bookmark

---
+++ rc/bookmarks.zsh
@@ -0,0 +1,35 @@
+# -*- sh -*-
+
+# Handle bookmarks. Similar to jump in oh-my-zsh:
+#  https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/jump/jump.plugin.zsh
+
+MARKPATH=$ZSH/run/marks
+
+g() {
+    # cd to the right mark. `-P` is to avoid zsh to hide the real directory.
+    cd -P $MARKPATH/$1
+}
+
+bookmark() {
+    if (( $# == 0 )); then
+        # Display bookmarks
+        for link in $MARKPATH/*(@); do
+            local markname="$fg[green]${link:t}$reset_color"
+	    local markpath="$fg[blue]$(readlink $link)$reset_color"
+	    printf "%20s\t-> %s\n" $markname $markpath
+	done
+    else
+        # Bookmark using the first argument as name
+        [[ -d $MARKPATH ]] || mkdir -p $MARKPATH
+        ln -s $PWD $MARKPATH/$1
+    fi
+}
+
+_g() {
+    local -a results
+    results=($MARKPATH/*(@N:t))
+    _wanted bookmark expl 'bookmarked directory' compadd -a \
+	results
+}
+
+compdef _g g
