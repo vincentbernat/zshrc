f463cefe7625bfe55df613472c93599176ac8a53
Vincent Bernat <vincent@bernat.im>
2014-05-23 02:54:54 +0200

    syntax: add syntax highlighting for zsh

--- plugins/syntax.plugin.zsh
+++
@@ -1,4 +0,0 @@
-# -*- sh -*-
-
-[[ -f ${0:h}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && \
-   source ${0:h}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
---
+++ plugins/zsh-syntax-highlighting.plugin.zsh
@@ -0,0 +1,1 @@
+../third-party/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
