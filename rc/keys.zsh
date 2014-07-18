7a3fc90dc9dd63b239c6a891783401bb045a1ea4
Vincent Bernat <vincent@bernat.im>
2014-07-18 19:54:41 +0200

    keys: add some nifty zle widgets to insert a previous word in history

--- rc/keys.zsh
+++ rc/keys.zsh
@@ -5,3 +5,12 @@ bindkey -e
 
 # Some systems misses the appropriate /etc/inputrc for this
 bindkey "\e[3~" delete-char        # Delete
+
+# Replace insert-last-word by a smart version
+autoload -U smart-insert-last-word
+zle -N insert-last-word smart-insert-last-word
+
+# Also copy previous word
+autoload -U copy-earlier-word
+zle -N copy-earlier-word
+bindkey "\e," copy-earlier-word
