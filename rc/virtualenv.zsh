1b91a0d89bf5eb765334a329efaa854b2eafe206
Vincent Bernat <vincent@bernat.im>
2014-07-30 16:26:01 +0200

    virtualenv: add support for opam (OCaml)

--- rc/virtualenv.zsh
+++ rc/virtualenv.zsh
@@ -154,7 +154,7 @@ EOF
     (( $+functions[deactivate_node] )) && deactivate_node
     (( $+functions[deactivate] )) && {
 	deactivate
-        restore GEM_HOME GEM_PATH GO_PATH LD_LIBRARY_PATH PKG_CONFIG_PATH
+        restore GEM_HOME GEM_PATH GO_PATH LD_LIBRARY_PATH PKG_CONFIG_PATH OPAMROOT
     }
 
     # Virtualenv
@@ -182,6 +182,10 @@ EOF
         export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
         export PKG_CONFIG_PATH=$VIRTUAL_ENV/lib/pkgconfig
         path=( $VIRTUAL_ENV/sbin $path )
+
+        # OCaml (through OPAM)
+        save OPAMROOT
+        export OPAMROOT=$VIRTUAL_ENV/opam
 
         rehash
         return
