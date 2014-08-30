4c5364c5b7a7df74cf827fc5d09e8e72acca912a
Vincent Bernat <vincent@bernat.im>
2014-08-30 16:59:15 +0200

    help: make run-help aware of git/openssl/sudo

--- rc/options.zsh
+++ rc/options.zsh
@@ -8,6 +8,10 @@ setopt extendedglob             # Enable extended globbing
 
 # meta-h will invoke man for the current command
 autoload run-help
+# When the command is {sudo,git,openssl} something, get help on something
+autoload run-help-sudo
+autoload run-help-git
+autoload run-help-openssl
 
 # Remove / from WORDCHARS (more like bash)
 WORDCHARS=${WORDCHARS:s#/#}
