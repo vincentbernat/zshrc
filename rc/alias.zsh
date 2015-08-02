8b5d4d1cb381a51439781f258a391e5ccaccca41
Vincent Bernat <vincent@bernat.im>
2015-08-02 20:33:22 +0200

    alias: move content of `colors.zsh` in `alias.zsh`
    It made little sense to keep them here.

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -11,6 +11,14 @@ else
   alias dmesg='dmesg -T'
 fi
 
+# ls colors
+(( ${terminfo[colors]:-0} >= 8 )) && {
+    export LSCOLORS="Gxfxcxdxbxegedabagacad"
+    ls --color -d . &>/dev/null && alias ls='ls --color=tty' || {
+        ls -G &> /dev/null && alias ls='ls -G'
+    }
+}
+
 # ip aliases
 alias ip6='ip -6'
 alias ipr='ip -r'
@@ -284,10 +292,33 @@ function \$() {
   "$@"
 }
 
+# Get my own external IP
 function myip() {
   false || \
       curl -s ip.appspot.com || \
       curl -s eth0.me || \
       curl -s ipecho.net/plain ||
       dig +short myip.opendns.com @resolver1.opendns.com
+}
+
+# Display a color testcard
+# From: http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+colortest() {
+    local T='gYw'   # The test text
+
+    local fg
+    local bg
+    printf "\n%12s" ""
+    for fg in {40..47}; do
+	printf "%7sm" ${fg}
+    done
+    printf "\n"
+    for fg in 0 1 $(for i in {30..37}; do echo $i 1\;$i; done); do
+	printf " %5s \e[%s  %s  " ${fg}m ${fg}m ${T}
+	for bg in {40..47}m; do
+	    printf " \e[%s\e[%s  %s  \e[0m" ${fg}m ${bg} ${T}
+	done
+	printf "\n"
+    done
+    printf "\n"
 }
