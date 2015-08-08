19bac1472ade23237a5bb4e64da3b810c974b999
Vincent Bernat <vincent@bernat.im>
2015-08-08 19:54:22 +0200

    terminfo: remove update of terminfo database
    Not using urxvt any more and world changed a bit. This seems useless to
    do that.

--- rc/02-terminfo.zsh
+++ rc/02-terminfo.zsh
@@ -1,18 +1,5 @@
 # -*- sh -*-
 
-# Update terminfo
-__() {
-    local terminfo
-    local termpath
-    for terminfo in $ZSH/terminfo/*.terminfo(.N); do
-	# We assume that the file is named appropriately for this to work
-	termpath=~/.terminfo/${(@)${terminfo##*/}[1]}/${${terminfo##*/}%%.terminfo}
-	if [[ ! -e $termpath ]] || [[ $terminfo -nt $termpath ]]; then
-	    TERMINFO=~/.terminfo tic $terminfo
-	fi
-    done
-} && __
-
 # Update TERM if we have LC__ORIGINALTERM variable
 # Also, try a sensible term where we have terminfo stuff
 autoload -U zsh/terminfo zsh/termcap
