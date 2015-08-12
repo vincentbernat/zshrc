3dea891f10cd94c7d7471f1e24b8cf4717c88c29
Vincent Bernat <vincent@bernat.im>
2015-08-12 10:30:00 +0200

    completion: make approximate depend on the current word length

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -27,7 +27,7 @@ zstyle ':completion:*:descriptions' format '%B%d%b'
 zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*:match:*' original only
-zstyle ':completion:*:approximate:*' max-errors 1 numeric
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX + $#SUFFIX) / 3 )) )'
 
 # Host completion
 _custom_hosts() {
