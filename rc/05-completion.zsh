c0cab08b76207e8fd25b17460507f35a04458763
Vincent Bernat <vincent@bernat.im>
2015-08-12 10:30:35 +0200

    completion: misc simplification of zstyles

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -14,18 +14,18 @@ setopt always_to_end
 setopt glob_complete
 unsetopt list_beep
 
+zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt ''
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:kill:*' force-list always
-zstyle ':completion:*:*:*:*:processes' command "ps -eo pid,user,comm,cmd -w -w"
+zstyle ':completion:*:processes' command "ps -eo pid,user,comm,cmd -w -w"
 zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH/run/u/$HOST-$UID/cache/
 zstyle ':completion:*:descriptions' format '%B%d%b'
 zstyle ':completion:*:functions' ignored-patterns '_*'
-zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*:match:*' original only
 zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX + $#SUFFIX) / 3 )) )'
 
