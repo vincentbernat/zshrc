c44c708b5deea086393a47e9abe8160758d35aff
Vincent Bernat <vincent@bernat.im>
2015-12-25 23:37:20 +0100

    locales: ensure LC_NUMERIC is en_US or C
    Use of fr_FR is bothersome.

--- rc/01-locale.zsh
+++ rc/01-locale.zsh
@@ -5,7 +5,8 @@
     local locales
     local locale
     locales=( "LANG fr_FR.utf8 fr_FR.UTF-8 en_US.utf8 en_US.UTF-8 C.utf8 C.UTF-8 C" \
-	      "LC_MESSAGES en_US.utf8 en_US.UTF-8 fr_FR.utf8 fr_FR.UTF-8 C.utf8 C.UTF-8 C" )
+	      "LC_MESSAGES en_US.utf8 en_US.UTF-8 fr_FR.utf8 fr_FR.UTF-8 C.utf8 C.UTF-8 C"
+	      "LC_NUMERIC en_US.utf8 en_US.UTF-8 C.utf8 C.UTF-8 C" )
     available=("${(f)$(locale -a)}")
     for locale in $locales; do
 	for l in $=locale[(w)2,-1]; do
