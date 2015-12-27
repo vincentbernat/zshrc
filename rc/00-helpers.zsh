8df1d5178e2e35250519c1e22aa1a81c907bb150
Vincent Bernat <vincent@bernat.im>
2015-12-27 11:36:10 +0100

    term: better unicode support detection
    We need to account for the missing locale too.

--- rc/00-helpers.zsh
+++ rc/00-helpers.zsh
@@ -14,8 +14,17 @@ _vbe_autoload is-at-least || is-at-least() { return 0 }
 
 # Test for unicode support
 _vbe_can_do_unicode () {
+    # We need:
+    #  1. at least zsh 4.3.4
+    #  2. multibyte input support
+    #  3. locale support
+    #  4. terminal support
+    # Locale support is tested by trying to output an unicode
+    # character. zsh will choke with "character not in range" if this
+    # doesn't work.
     if is-at-least 4.3.4 && \
-           [[ -o multibyte ]] && (( ${#${:-↵}} == 1 )); then
+            [[ -o multibyte ]] && \
+            (( ${#${:-$(print -n "\u21B5" 2> /dev/null)}} == 1 )); then
         case $TERM in
             screen*) ;;
             xterm*) ;;
