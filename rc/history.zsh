88df7bedfe669e1301c05510f8aed13dbfe98381
Vincent Bernat <vincent@bernat.im>
2016-03-06 10:33:58 +0100

    history: ignore commands starting with space
    This way, if you want to keep a command out of history, prefix it with
    space.

--- rc/history.zsh
+++ rc/history.zsh
@@ -3,6 +3,7 @@
 setopt extended_history	        # save timestamps
 setopt share_history            # share history accross zsh sessions
 setopt hist_ignore_all_dups	# ignores duplicates
+setopt hist_ignore_space        # don't store commands starting with a space
 
 HISTFILE=$ZSH/run/u/$HOST-$UID/history
 HISTSIZE=20000
