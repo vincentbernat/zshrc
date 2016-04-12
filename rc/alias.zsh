4f61926ccd8f4f5cd675cf3f4fcb9960e47f1f58
Vincent Bernat <vincent@bernat.im>
2016-04-12 09:35:36 +0200

    alias: setup less colors only if we support colors

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -32,13 +32,15 @@ alias ipm='ip -r monitor'
 (( $+commands[cloudstack] )) && alias cs=cloudstack
 
 # Setting up less colors
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[7;32;5;246m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[04;38;5;146m'
+(( ${terminfo[colors]:-0} >= 8 )) && {
+  export LESS_TERMCAP_mb=$'\E[01;31m'
+  export LESS_TERMCAP_md=$'\E[01;38;5;74m'
+  export LESS_TERMCAP_me=$'\E[0m'
+  export LESS_TERMCAP_se=$'\E[0m'
+  export LESS_TERMCAP_so=$'\E[7;32;5;246m'
+  export LESS_TERMCAP_ue=$'\E[0m'
+  export LESS_TERMCAP_us=$'\E[04;38;5;146m'
+}
 
 # grep
 __() {
