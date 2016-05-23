ab71dcec275aad438a1ee7007f576c56b0114724
Vincent Bernat <vincent@bernat.im>
2016-05-23 19:33:03 +0200

    virtualenv: add virtualenv2/3 aliases
    virtualenv will be virtualenv3 is python 3 is installed.

--- rc/virtualenv.zsh
+++ rc/virtualenv.zsh
@@ -7,20 +7,33 @@
 
 WORKON_HOME=${WORKON_HOME:-~/.virtualenvs}
 
-(( $+commands[virtualenv] )) && virtualenv () {
-    [[ -d $WORKON_HOME ]] || mkdir -p $WORKON_HOME
-    pushd $WORKON_HOME > /dev/null && {
-	command virtualenv "$@" && \
-            cat <<EOF >&2
+(( $+commands[virtualenv] )) && {
+    _virtualenv () {
+        local interpreter
+        case $1 in
+            2) interpreter=python2 ;;
+            3) interpreter=python3 ;;
+            *) interpreter=python ;;
+        esac
+        shift
+        [[ -d $WORKON_HOME ]] || mkdir -p $WORKON_HOME
+        pushd $WORKON_HOME > /dev/null && {
+	    command virtualenv -p =$interpreter "$@" && \
+                cat <<EOF >&2
 ${fg[white]}
 # To reuse the environment for Node.JS, use:
 #  \$ pip install nodeenv
 #  \$ nodeenv -p -n system
 
 EOF
-	popd > /dev/null
+	    popd > /dev/null
+        }
+        workon ${@[-1]}
     }
-    workon ${@[-1]}
+    alias virtualenv2='_virtualenv 2'
+    alias virtualenv3='_virtualenv 3'
+    (( $+commands[python2] )) && alias virtualenv='_virtualenv 2'
+    (( $+commands[python3] )) && alias virtualenv='_virtualenv 3'
 }
 
 (( $+commands[virtualenv] )) && {
