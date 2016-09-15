871be6da3098bc6df489fcca0ceca7c2717957a5
Vincent Bernat <vincent@bernat.im>
2016-09-15 16:14:51 +0200

    clean: do not clean ~/tmp
    This can be slow. Sloooooww.

--- rc/zz-clean.zsh
+++ rc/zz-clean.zsh
@@ -2,9 +2,3 @@
 
 # Remove old run files
 rm -f $ZSH/run/u/*/{history,editor,zcompdump,editor,bookmarks}-*(NU.aw+10) 2> /dev/null
-
-# Clean files in tmp
-[ -d ~/tmp ] && {
-    rm -f ~/tmp/**/*(NU.cw+3)
-    rmdir ~/tmp/**/*(NU/cw+3^F)
-}  2> /dev/null
