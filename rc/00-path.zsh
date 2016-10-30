5c0cf0590a90a172e1eafb0c79ab5c441cdaa3a9
Vincent Bernat <vincent@bernat.im>
2016-10-30 09:22:00 +0100

    path: keep not-known directories first
    If we override PATH with some commands, we would like them to be
    first. Therefore, order those first except if we know them, in this
    case, order them as we expect.

--- rc/00-path.zsh
+++ rc/00-path.zsh
@@ -3,10 +3,18 @@
 __() {
     local -a wanted savedpath
     local p
-    wanted=(~/bin /usr/lib/ccache /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin)
+    wanted=(~/bin /usr/lib/ccache
+            /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+            /usr/local/games /usr/games)
     savedpath=($path)
     path=()
-    for p in $wanted $savedpath; do
+    for p in $savedpath; do
+        p=${p:A}
+	(( ${${wanted[(r)$p]}:+1} )) || (( ${${path[(r)$p]}:+1} )) || {
+	    [ -d $p ] && path=($path $p)
+	}
+    done
+    for p in $wanted; do
         p=${p:A}
 	(( ${${path[(r)$p]}:+1} )) || {
 	    [ -d $p ] && path=($path $p)
