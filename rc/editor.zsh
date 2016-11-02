7e276f92a812b6102d42517778842957569e0154
Vincent Bernat <vincent@bernat.im>
2016-11-02 23:42:15 +0100

    editor: prevent backup files even when not running as root
    We don't want to read initialization files when running as root (too
    dangerous). However, we want font locking and no backup files whenever
    we are root or not (most remote hosts don't have the appropriate
    configuration file).

--- rc/editor.zsh
+++ rc/editor.zsh
@@ -6,9 +6,9 @@ __() {
     local editor
     editors=(
         # Don't use "emacs" as it could be an alternative to something else (like jove)
-	"emacs25 -nw ${(%):-%(!.-q --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'.)}" # emacs
-	"emacs24 -nw ${(%):-%(!.-q --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'.)}" # emacs
-	"emacs23 -nw ${(%):-%(!.-q --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'.)}" # emacs
+	"emacs25 -nw ${(%):-%(!.-q.)} --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'" # emacs
+	"emacs24 -nw ${(%):-%(!.-q.)} --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'" # emacs
+	"emacs23 -nw ${(%):-%(!.-q.)} --eval='(global-font-lock-mode 1) (setq make-backup-files nil)'" # emacs
         "mg -n"           # emacs clone (make it not create backup files)
         "jove"            # Another emacs clone (don't create backup files by default)
 	"zile" "jed"      # Other emacs clone (create backup files by default)
