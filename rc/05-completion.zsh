8b6399056415e4306c42b2a49b0d9b48c7aaaf6e
Vincent Bernat <vincent@bernat.im>
2016-12-12 10:03:44 +0100

    completion: don't display dots when completing
    This is broken with ZSH 5.3. No time to debug.

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -63,12 +63,3 @@ zstyle ":completion:*:hosts" known-hosts-files ''
 # In menu, select items with +
 zmodload -i zsh/complist
 bindkey -M menuselect "+" accept-and-menu-complete
-
-# Display dots when completion is in progress
-expand-or-complete-with-dots() {
-    echo -n "\e[31m...\e[0m"
-    zle expand-or-complete
-    zle redisplay
-}
-zle -N expand-or-complete-with-dots
-bindkey "^I" expand-or-complete-with-dots
