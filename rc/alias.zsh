33bfa93bca67da223566adb5cd551d20ae35cdf8
Vincent Bernat <vincent@bernat.im>
2018-03-21 07:14:39 +0100

    alias: fix LESS_TERMCAP to not blink

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -44,7 +44,7 @@ alias tailf='tail -F'           # not shipped in util-linux anymore
   export LESS_TERMCAP_md=$'\E[1;38;5;74m'
   export LESS_TERMCAP_me=$'\E[0m'
   export LESS_TERMCAP_se=$'\E[0m'
-  export LESS_TERMCAP_so=$'\E[1;3;5;246m'
+  export LESS_TERMCAP_so=$'\E[1;3;246m'
   export LESS_TERMCAP_ue=$'\E[0m'
   export LESS_TERMCAP_us=$'\E[1;32m'
 }
