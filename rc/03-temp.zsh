dcd0253f23029f8523cbd64c6a7e6c36d2f60b3b
Vincent Bernat <vincent@bernat.im>
2018-05-13 20:24:42 +0200

    temp: inside nix-shell, fix temporary directory value

--- rc/03-temp.zsh
+++ rc/03-temp.zsh
@@ -3,3 +3,12 @@
 # Create a temporary directory
 [ ! -h ~/tmp ] && [ ! -d ~/tmp ] && [ -w ~ ] && \
     mkdir ~/tmp
+
+[[ -n $IN_NIX_SHELL ]] && () {
+    # Nix configure temporary directory to /run/user/$UID. Why?
+    local v
+    for v in TMP TMPDIR TEMP TEMPDIR; do
+        [[ ${(P)v} = "/run/user/$UID" ]] && \
+            export $v=${:-~/tmp}
+    done
+}
