cf13a2b67813a2c7fcd9dc3e9158892b6ed2f6d0
Vincent Bernat <vincent@bernat.ch>
2019-08-03 14:56:12 +0200

    ssh: remove rssh function
    I don't use that anymore.

--- rc/ssh.zsh
+++ rc/ssh.zsh
@@ -48,24 +48,6 @@ ssh() {
     esac
 }
 
-# The following command implements a reverse SSH connection. This is
-# to connect to hosts behind a firewall, which can connect to your
-# machine but you cannot connect directly. The idea is that they issue
-# a TCP connection that you will use as a tunnel to access their SSH
-# port.
-#
-# I am using this to connect to VM using user-mode network (QEMU, KVM,
-# UML, ...).
-rssh() {
-    # We should probe for a free port, but is it easy?
-    local port
-    port=$((21422 + $RANDOM % 1000))
-
-    print "On remote host, use \`socat TCP:10.0.2.2:$port TCP:127.0.0.1:22\` to allow SSH access... "
-    ssh -oProxyCommand="socat TCP-LISTEN:$port,bind=127.0.0.1,reuseaddr STDIO" \
-        "$@"
-}
-
 # Invoke this shell on a remote host. All arguments are passed to SSH,
 # but we expect to use this for interactive shells only. Several
 # connections may be needed to install the appropriate files. It
