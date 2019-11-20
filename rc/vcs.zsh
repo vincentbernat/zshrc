a161d70d2a52248f23a48fa9e59e90b5c9a9d340
Vincent Bernat <vincent@bernat.ch>
2019-11-20 08:31:32 +0100

    vcs: on fuse-like FS, keep VCS info but don't query expensive stuff

--- rc/vcs.zsh
+++ rc/vcs.zsh
@@ -24,11 +24,12 @@
 	zstyle ':vcs_info:*:*'   actionformats ${common}"%{${fg[default]}%} ${PRCH[sep]} %{${fg[green]}%}"%a
 	zstyle ':vcs_info:*:*'   stagedstr     "%{${fg[green]}%}${PRCH[circle]}"
 	zstyle ':vcs_info:*:*'   unstagedstr   "%{${fg[yellow]}%}${PRCH[circle]}"
-	zstyle ':vcs_info:*:*'   check-for-changes true
+	zstyle -e ':vcs_info:*:*'   check-for-changes '[[ $(zstat +blocks $PWD) -ne 0 ]] && reply=( true ) || reply=( false )'
 
         zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
         +vi-git-untracked(){
+            [[ $(zstat +blocks $PWD) -ne 0 ]] || return
             if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
                 git status --porcelain 2> /dev/null | grep -q '??' ; then
                 hook_com[staged]+="%{${fg[black]}%}${PRCH[circle]}"
@@ -44,24 +45,14 @@
         async_start_worker vcs_info
         async_register_callback vcs_info _vbe_vcs_info_done
         add-zsh-hook precmd (){
-            # Heuristic to check for a fuse-like filesystem
-            if [[ $(zstat +blocks $PWD) -ne 0 ]]; then
-                async_job vcs_info _vbe_vcs_info $PWD
-            else
-                vcs_info_msg_0_=
-            fi
+            async_job vcs_info _vbe_vcs_info $PWD
         }
         add-zsh-hook chpwd (){
             [[ -z $vcs_info_msg_0_ ]] ||
                 vcs_info_msg_0_="$vcs_info_msg_0_${PRCH[ellipsis]}"
         }
     else
-        add-zsh-hook precmd (){
-            # Heuristic to check for a fuse-like filesystem
-            if [[ $(zstat +blocks $PWD) -ne 0 ]]; then
-                vcs_info
-            fi
-        }
+        add-zsh-hook precmd vcs_info
     fi
 
     _vbe_add_prompt_vcs () {
