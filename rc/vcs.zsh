d9d01c0c02582b19e22bf23eae7d60d1a673b3bf
Vincent Bernat <vincent@bernat.ch>
2019-11-21 02:57:41 +0100

    vcs: workaround for dead worker

--- rc/vcs.zsh
+++ rc/vcs.zsh
@@ -5,13 +5,28 @@
 [[ $USERNAME != "root" ]] && {
 
     # Async helpers
+    _vbe_vcs_async_start() {
+        async_start_worker vcs_info
+        async_register_callback vcs_info _vbe_vcs_info_done
+    }
     _vbe_vcs_info() {
         cd -q $1
         vcs_info
         print ${vcs_info_msg_0_}
     }
     _vbe_vcs_info_done() {
-        vcs_info_msg_0_="$3"
+        local job=$1
+        local return_code=$2
+        local stdout=$3
+        if [[ $job == '[async]' ]]; then
+            if [[ $return_code -eq 2 ]]; then
+                # Need to restart the worker. Stolen from
+                # https://github.com/mengelbrecht/slimline/blob/master/lib/async.zsh
+                _vbe_vcs_async_start
+                return
+            fi
+        fi
+        vcs_info_msg_0_=$stdout
         zle reset-prompt
     }
 
@@ -42,8 +57,7 @@
     if is-at-least 5.2; then
         source $ZSH/third-party/async.zsh
         async_init
-        async_start_worker vcs_info
-        async_register_callback vcs_info _vbe_vcs_info_done
+        _vbe_vcs_async_start
         add-zsh-hook precmd (){
             async_job vcs_info _vbe_vcs_info $PWD
         }
