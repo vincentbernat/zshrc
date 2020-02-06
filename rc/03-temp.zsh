c9e1fb0b25f9bac7a0f915221289e40c24be2a87
Vincent Bernat <vincent@bernat.ch>
2020-02-06 10:41:14 +0100

    temp: do not create a temporary directory
    This seems a bit rude on shared accounts. And I cannot detect a shared account.

--- rc/03-temp.zsh
+++ rc/03-temp.zsh
@@ -1,9 +1,5 @@
 # -*- sh -*-
 
-# Create a temporary directory
-[ ! -h ~/tmp ] && [ ! -d ~/tmp ] && [ -w ~ ] && [[ -z $SUDO_USER ]] && \
-    mkdir ~/tmp
-
 [[ -n $IN_NIX_SHELL ]] && () {
     # Nix configure temporary directory to /run/user/$UID. Why?
     local v
