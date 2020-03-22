acbdba294e99ebc12e76da6c56c08d5f538d720d
Vincent Bernat <vincent@bernat.ch>
2020-03-22 11:49:17 +0100

    virtualenv: virtualenv command may not be installed
    But, we can invoke the module. This is a bit odd.

--- rc/virtualenv.zsh
+++ rc/virtualenv.zsh
@@ -5,18 +5,17 @@
 
 WORKON_HOME=${WORKON_HOME:-~/.virtualenvs}
 
-(( $+commands[virtualenv] )) && {
-    _virtualenv () {
-        local interpreter
-        case $1 in
-            2) interpreter=python2 ;;
-            3) interpreter=python3 ;;
-            *) interpreter=python ;;
-        esac
-        shift
-        [[ -d $WORKON_HOME ]] || mkdir -p $WORKON_HOME
-        pushd $WORKON_HOME > /dev/null || return
-	! command virtualenv -p =$interpreter "$@" || \
+_virtualenv () {
+    local interpreter
+    case $1 in
+        2) interpreter=python2 ;;
+        3) interpreter=python3 ;;
+        *) interpreter=python ;;
+    esac
+    shift
+    [[ -d $WORKON_HOME ]] || mkdir -p $WORKON_HOME
+    pushd $WORKON_HOME > /dev/null || return
+    ! command $interpreter -m virtualenv -p =$interpreter "$@" || \
             cat <<EOF >&2
 ${fg[white]}
 # To reuse the environment for Node.JS, use:
@@ -28,16 +27,16 @@ EOF
         workon ${@[-1]}
         [[ ${@[-1]} == "tmp" ]] && \
             rm -rf $WORKON_HOME/tmp
-    }
-    alias virtualenv2='_virtualenv 2'
-    alias virtualenv3='_virtualenv 3'
-    (( $+commands[python2] )) && alias virtualenv='_virtualenv 2'
-    (( $+commands[python3] )) && alias virtualenv='_virtualenv 3'
+}
 
-    hash -d venvs=$WORKON_HOME
-    _vbe_add_prompt_virtualenv () {
-        _vbe_prompt_env 've' '${VIRTUAL_ENV##*/}'
-    }
+alias virtualenv2='_virtualenv 2'
+alias virtualenv3='_virtualenv 3'
+(( $+commands[python2] )) && alias virtualenv='_virtualenv 2'
+(( $+commands[python3] )) && alias virtualenv='_virtualenv 3'
+
+hash -d venvs=$WORKON_HOME
+_vbe_add_prompt_virtualenv () {
+    _vbe_prompt_env 've' '${VIRTUAL_ENV##*/}'
 }
 
 autoload -Uz workon
