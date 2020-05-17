5daa3a1d4a073ebf12da5c38c275a1f1dd910c2f
Vincent Bernat <vincent@bernat.ch>
2020-05-17 13:06:18 +0200

    alias: use xdg-app-chooser if available

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -50,8 +50,9 @@ alias tailf='tail -F'           # not shipped in util-linux anymore
 
 # Other simple aliases
 (( $+commands[xdg-open] )) && alias o=xdg-open
+(( $+commands[xdg-app-chooser] )) && alias o=xdg-app-chooser
 (( $+commands[irb] )) && alias irb='irb --readline -r irb/completion'
-(( $+commands[ipython] )) && alias ipython2==ipython # maybe inexact
+(( $+commands[ipython] )) && alias ipython2=\=ipython # maybe inexact
 (( $+commands[ipython3] )) && alias ipython=ipython3
 (( $+commands[pip] )) && alias pip='PIP_REQUIRE_VIRTUALENV=true pip --disable-pip-version-check'
 (( $+commands[pip3] )) && alias pip3='PIP_REQUIRE_VIRTUALENV=true pip3 --disable-pip-version-check'
