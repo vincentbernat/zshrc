924405a85887792c9a4fec88f605efdf836596b9
Vincent Bernat <vincent@bernat.ch>
2020-09-05 21:50:53 +0200

    alias: make nix-zsh a simpler alias
    It seems we don't need glibcLocales anymore. Let's try without. This
    change a bit the interface as we need to use `-p` if we want to add
    packages, but this allows us to provide a custom shell.nix.

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -67,10 +67,7 @@ alias tailf='tail -F'           # not shipped in util-linux anymore
 alias clear='clear && [[ -n $TMUX ]] && tmux clear-history || true'
 mkcd() { command mkdir -p $1 && cd $1 }
 (( $+commands[nix-shell] )) && nix-zsh() {
-        case ${#${@:#-*}} in
-            0) nix-shell --command zsh "$@" ;;
-            *) nix-shell --command zsh -p glibcLocales "$@" ;;
-        esac
+        nix-shell --command zsh "$@"
 }
 
 # Global aliases. I am using `,' as a prefix.
