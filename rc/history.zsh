a9232001cc59f3c2a1a69f983a4a7bb901d07acf
Vincent Bernat <vincent@bernat.ch>
2020-12-30 09:01:00 +0100

    history: tentative to share history

--- rc/history.zsh
+++ rc/history.zsh
@@ -5,7 +5,33 @@ setopt share_history            # share history accross zsh sessions
 setopt hist_ignore_all_dups	# ignores duplicates
 setopt hist_ignore_space        # don't store commands starting with a space
 
-HISTFILE=$ZSH/run/u/$HOST-$UID/history
+HISTFILE=
+() {
+    local histfile
+    for histfile in (
+        ~/.zsh_history
+        ~/.zsh/run/u/$HOST-$UID/history
+        $ZSH/run/u/$HOST-$UID/history
+    ); do
+        if [[ -z HISTFILE ]]; then
+            HISTFILE=$histfile
+            continue
+        fi
+        if [[ $histfile -ef $HISTFILE ]]; then
+            continue
+        fi
+        if [[ -O $HISTFILE ]] && [[ -O $histfile ]]; then
+            if [[ $histfile -ot $HISTFILE ]]; then
+                fc -R $histfile
+                rm -f $histfile
+                ln -s $HISTFILE $histfile
+                continue
+            fi
+        fi
+        HISTFILE=$histfile
+    done
+}
+
 HISTSIZE=30000
 SAVEHIST=30000
 
