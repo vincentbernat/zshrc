18ae16231a639260f3eb56047667c5116caf4ee9
Vincent Bernat <vincent@bernat.ch>
2020-12-30 09:14:37 +0100

    history: share history with other users depending on hostname

--- rc/history.zsh
+++ rc/history.zsh
@@ -5,33 +5,16 @@ setopt share_history            # share history accross zsh sessions
 setopt hist_ignore_all_dups	# ignores duplicates
 setopt hist_ignore_space        # don't store commands starting with a space
 
-HISTFILE=
-() {
-    local histfile
-    for histfile in (
-        ~/.zsh_history
-        ~/.zsh/run/u/$HOST-$UID/history
-        $ZSH/run/u/$HOST-$UID/history
-    ); do
-        if [[ -z HISTFILE ]]; then
-            HISTFILE=$histfile
-            continue
+case $HOSTNAME in
+    *.blade-group.net)
+        HISTFILE=~/.zsh_history
+        if [[ -O $ZSH/run/u/$HOST-$UID/history ]]; then
+            fc -R $ZSH/run/u/$HOST-$UID/history
+            rm -f $ZSH/run/u/$HOST-$UID/history
         fi
-        if [[ $histfile -ef $HISTFILE ]]; then
-            continue
-        fi
-        if [[ -O $HISTFILE ]] && [[ -O $histfile ]]; then
-            if [[ $histfile -ot $HISTFILE ]]; then
-                fc -R $histfile
-                rm -f $histfile
-                ln -s $HISTFILE $histfile
-                continue
-            fi
-        fi
-        HISTFILE=$histfile
-    done
-}
-
+        ;;
+    *) HISTFILE=$ZSH/run/u/$HOST-$UID/history ;;
+esac
 HISTSIZE=30000
 SAVEHIST=30000
 
