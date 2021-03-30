ab18be44ce54fa26bb8924bec6c80012d9283fd7
Vincent Bernat <vincent@bernat.ch>
2021-03-30 08:49:00 +0200

    history: compute domain name
    The idea was to anonymize it in some way but there is not builtin hash
    function in Zsh that could help.

--- rc/history.zsh
+++ rc/history.zsh
@@ -5,17 +5,20 @@ setopt share_history            # share history accross zsh sessions
 setopt hist_ignore_all_dups	# ignores duplicates
 setopt hist_ignore_space        # don't store commands starting with a space
 
-case $HOSTNAME in
-    *.blade-group.net)
-        HISTFILE=~/.zsh_history
-        if [[ -O $ZSH/run/u/$HOST-$UID/history ]]; then
-            fc -R $ZSH/run/u/$HOST-$UID/history
-            fc -A
-            rm -f $ZSH/run/u/$HOST-$UID/history
-        fi
-        ;;
-    *) HISTFILE=$ZSH/run/u/$HOST-$UID/history ;;
-esac
+() {
+    local domain=${(pj:.:)${(ps:.:)HOSTNAME}[-2,-1]}
+    case $domain in
+        blade-group.net)
+            HISTFILE=~/.zsh_history
+            if [[ -O $ZSH/run/u/$HOST-$UID/history ]]; then
+                fc -R $ZSH/run/u/$HOST-$UID/history
+                fc -A
+                rm -f $ZSH/run/u/$HOST-$UID/history
+            fi
+            ;;
+        *) HISTFILE=$ZSH/run/u/$HOST-$UID/history ;;
+    esac
+}
 HISTSIZE=30000
 SAVEHIST=30000
 
