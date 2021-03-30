25bcd4ae642548965d32e6c0233bd3078bc37f4e
Vincent Bernat <vincent@bernat.ch>
2021-03-31 00:27:55 +0200

    history: hash domain to hide specific locations I may use

--- rc/history.zsh
+++ rc/history.zsh
@@ -7,8 +7,9 @@ setopt hist_ignore_space        # don't store commands starting with a space
 
 () {
     local domain=${(pj:.:)${(ps:.:)HOSTNAME}[-2,-1]}
-    case $domain in
-        blade-group.net)
+    local hashed=${"$(print Ahyeigh4yo $domain | ${commands[sha256sum]:-$commands[sha256]} 2> /dev/null)"[1,16]}
+    case $hashed in
+        49e78cdf67755528)
             HISTFILE=~/.zsh_history
             if [[ -O $ZSH/run/u/$HOST-$UID/history ]]; then
                 fc -R $ZSH/run/u/$HOST-$UID/history
@@ -16,6 +17,7 @@ setopt hist_ignore_space        # don't store commands starting with a space
                 rm -f $ZSH/run/u/$HOST-$UID/history
             fi
             ;;
+        12e640a29535f352) HISTFILE=~/.zhistory ;;
         *) HISTFILE=$ZSH/run/u/$HOST-$UID/history ;;
     esac
 }
