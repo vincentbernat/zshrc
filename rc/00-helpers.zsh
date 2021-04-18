fd658e47757fa4ae8e1d7c34490c3fcf4c0bda96
Vincent Bernat <vincent@bernat.ch>
2021-04-18 11:26:20 +0200

    helpers: fix unicode detection
    We need to ensure we can detect the proper width of characters.

--- rc/00-helpers.zsh
+++ rc/00-helpers.zsh
@@ -16,16 +16,17 @@ zmodload zsh/datetime           # EPOCHSECONDS
 _vbe_can_do_unicode () {
     # We need:
     #  1. multibyte input support
-    #  2. locale support
+    #  2. locale support + correct width
     #  3. terminal support
+    #
     # Locale support is tested by trying to output an unicode
-    # character. zsh will choke with "character not in range" if this
-    # doesn't work.
-    if [[ -o multibyte ]] \
-       && (( ${#${:-$(print -n "\u21B5\u21B5" 2> /dev/null)}} == 2 )); then
-        case $TERM in
-            screen*|xterm*|rxvt*) return 0 ;;
-        esac
-    fi
+    # character. Zsh will choke with "character not in range" if this
+    # doesn't work. Correct width is checked by asking Zsh to pad a
+    # recent double-width unicode character. Both tests are combined.
+    [[ -o multibyte ]] || return 1
+    (( ${#${(ml[4])${:-$(print -n "\U1f40b" 2> /dev/null)}}} == 3 )) || return 1
+    case $TERM in
+        screen*|xterm*|rxvt*) return 0 ;;
+    esac
     return 1
 }
