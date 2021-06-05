5b58cd4f5031a81c0ba8434378361701dc3a11b7
Vincent Bernat <vincent@bernat.ch>
2021-06-05 20:50:20 +0200

    paste: no need to restrict bracketed paste to some terminals
    Zsh does not do that. Just allow for everything.

--- rc/paste.zsh
+++ rc/paste.zsh
@@ -12,7 +12,6 @@
 #    bind ] paste-buffer -p
 
 (( $+zle_bracketed_paste == 0 )) \
-    && [[  $TERM =~ ^(rxvt-unicode|xterm|screen)(-256color|-direct)?$ ]] \
     && () {
 
     # create a new keymap to use while pasting
