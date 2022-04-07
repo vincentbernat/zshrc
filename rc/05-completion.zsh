274991f996c87af83c346752b4fba312e6dece9f
Vincent Bernat <vincent@bernat.ch>
2022-04-07 23:49:24 +0200

    completion: fix compinit invocation
    - last access file may never be created
    - ">" alone invokes cat as a command

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -11,9 +11,9 @@
     local attempts=30
     while (( attempts-- > 0 )) && ! ln $zcd $zcdl 2> /dev/null; do sleep 0.1; done
     {
-        if [[ -n $zcda(#qN.mh+24) ]]; then
+        if [[ ! -e $zcda || -n $zcda(#qN.mh+24) ]]; then
             compinit -i -d $zcd
-            > $zcda
+            : > $zcda
         else
             compinit -C -d $zcd
         fi
