429c446e36f4aa380706c200d62bc3eb41c1b33c
Vincent Bernat <vincent@bernat.ch>
2022-06-05 14:52:24 +0200

    options: factor a bit autoloading for run-help
    We should look in fpath for run-help-* functions.

--- rc/options.zsh
+++ rc/options.zsh
@@ -19,11 +19,12 @@ fi
 
 # meta-h will invoke man for the current command
 autoload -Uz run-help
-# When the command is {sudo,git,openssl} something, get help on something
-autoload -Uz run-help-sudo
-autoload -Uz run-help-git
-autoload -Uz run-help-openssl
-autoload -Uz run-help-ip
+() {
+    local c
+    for c in sudo git openssl ip nix; do
+        (( $+commands[$c] )) && autoload -Uz run-help-$c
+    done
+}
 
 # Remove / from WORDCHARS (more like bash)
 WORDCHARS=${WORDCHARS:s#/#}
@@ -32,5 +33,5 @@ if (( ${termcap[Co]:-0} > 8)); then
     # Enable and configure autosuggest
     source $ZSH/third-party/zsh-autosuggestions.zsh
     typeset -g ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=50
-    ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste accept-line insert-last-word copy-earlier-word)
+    ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste accept-line insert-last-word copy-earlier-word run-help)
 fi
