d444a2d98b2caeeb6c1f192aeeda7e0af2206a9a
Vincent Bernat <vincent@bernat.ch>
2022-06-07 15:19:26 +0200

    tmux: make tmux-cssh use ssh function
    Also, add tmux-cpssh to use pssh

--- rc/tmux.zsh
+++ rc/tmux.zsh
@@ -3,7 +3,6 @@
 # tmux-related stuff
 
 (( $+commands[tmux] )) && [[ -n $TMUX ]] && {
-
     # Execute a command in a new pane and synchronise all panes. This
     # is a replacement of cluster-ssh. Here is how to execute it:
     #
@@ -14,14 +13,17 @@
         local window
         for host in "$@"; do
             if [[ -z $window ]]; then
-                window=$(tmux new-window -d -P -F '#{session_name}:#{window_index}' "ssh $host")
+                window=$(tmux new-window -d -P -F '#{session_name}:#{window_index}' "$SHELL --interactive -c '${SSH_COMMAND:-ssh} $host'")
             else
-                tmux split-window -t $window "ssh $host"
+                tmux split-window -t $window "$SHELL --interactive -c '${SSH_COMMAND:-ssh} $host'"
                 tmux select-layout -t $window tiled
             fi
         done
         tmux set-window-option -t $window synchronize-panes on
         tmux select-window -t $window
+    }
+    function tmux-cpssh() {
+        SSH_COMMAND=pssh tmux-cssh "$@"
     }
 
     # Slow pasting. First argument is tmux pane (X:Y.0)
@@ -32,5 +34,4 @@
             tmux send-keys -t "$target" -l "${line}"$'\n';
         done
     }
-
 }
