1a4387d7a0169504442d6c7d126a91d6c07cda1c
Vincent Bernat <vincent@bernat.ch>
2022-11-03 00:48:22 +0100

    direnv: initialize

--- rc/direnv.zsh
+++ rc/direnv.zsh
@@ -1,16 +1,17 @@
 # -*- sh -*-
 
 (( $+commands[direnv] )) && {
-    _direnv_hook() {
+    _vbe_direnv_hook() {
         trap -- '' SIGINT;
         eval "$(direnv export zsh)";
         trap - SIGINT;
     }
-    chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+    add-zsh-hook chpwd _vbe_direnv_hook
 
     _vbe_add_prompt_direnv () {
         [[ $DIRENV_STATUS == "allowed" ]] && _vbe_prompt_env $PRCH[envrc] ${DIRENV_DIR:t}
     }
 
     export DIRENV_LOG_FORMAT=""
+    _vbe_direnv_hook
 }
