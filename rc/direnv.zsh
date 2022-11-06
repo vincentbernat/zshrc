bcb185c5aa883cb159c8db97ab3c8408c3d65239
Vincent Bernat <vincent@bernat.ch>
2022-11-06 15:30:25 +0100

    direnv: also reexecute direnv when inside direnv
    Otherwise, the watch mechanism is useless.

--- rc/direnv.zsh
+++ rc/direnv.zsh
@@ -6,7 +6,12 @@
         eval "$(direnv export zsh)";
         trap - SIGINT;
     }
+    _vbe_direnv_maybe_hook() {
+        [[ $DIRENV_STATUS == "allowed" ]] || return
+        _vbe_direnv_hook
+    }
     add-zsh-hook chpwd _vbe_direnv_hook
+    add-zsh-hook precmd _vbe_direnv_maybe_hook
 
     _vbe_add_prompt_direnv () {
         [[ $DIRENV_STATUS == "allowed" ]] && _vbe_prompt_env $PRCH[envrc] ${DIRENV_DIR:t}
