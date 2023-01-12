b97f16c866e9c5ef3ce6083303d2342141d784bf
Vincent Bernat <vincent@bernat.ch>
2023-01-12 22:48:20 +0100

    locale: reformat

--- rc/01-locale.zsh
+++ rc/01-locale.zsh
@@ -1,24 +1,26 @@
 # -*- sh -*-
 
 (( $+commands[locale] )) && () {
-    local -a available
-    local -A locales
-    local locale l
-    locales=( "LANG" "en_US C"
-	      "LC_MESSAGES" "en_US C"
-              "LC_TIME" "C"
-	      "LC_NUMERIC" "en_US C" )
-    available=("${(f)$(locale -a)}")
-    for locale in ${(k)locales}; do
-        export $locale=C        # default value
-	for l in $=locales[$locale]; do
-            for charset in UTF-8 utf8; do
-                if (( ${available[(i)$l.$charset]} <= ${#available} )); then
-		    export $locale=$l.$charset
-		    break 2
-	        fi
-            done
-	done
-    done
-    export LC_CTYPE=$LANG
+  local -a available
+  local -A locales
+  local locale l
+  locales=(
+    "LANG" "en_US C"
+	  "LC_MESSAGES" "en_US C"
+    "LC_TIME" "C"
+	  "LC_NUMERIC" "en_US C"
+  )
+  available=("${(f)$(locale -a)}")
+  for locale in ${(k)locales}; do
+    export $locale=C        # default value
+	  for l in $=locales[$locale]; do
+      for charset in UTF-8 utf8; do
+        if (( ${available[(i)$l.$charset]} <= ${#available} )); then
+          export $locale=$l.$charset
+          break 2
+	      fi
+      done
+	  done
+  done
+  export LC_CTYPE=$LANG
 } 2> /dev/null
