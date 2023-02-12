161e275cdf5ea3c6a65d1a9f05e6c86b1f8a08b7
Vincent Bernat <vincent@bernat.ch>
2023-02-13 00:04:40 +0100

    alias: no need for braces for single-command functions

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -203,15 +203,13 @@ alias smv='rsync -P --remove-source-files'
 
 # Like sudo -sE but it preserves ZDOTDIR, see:
 #  <https://sources.debian.org/src/sudo/1.9.1-1/plugins/sudoers/env.c/?hl=187#L187>
-suzsh() {
-    command sudo -H -u ${1:-root} \
-        env ZDOTDIR=${ZDOTDIR:-$HOME} \
-        ZSH=$ZSH \
-        ${DISPLAY+DISPLAY=$DISPLAY} \
-        ${SSH_TTY+SSH_TTY=$SSH_TTY} \
-        ${SSH_AUTH_SOCK+SSH_AUTH_SOCK=$SSH_AUTH_SOCK} \
-        ${ZSH_NAME} -i -l
-}
+suzsh() command sudo -H -u ${1:-root} \
+    env ZDOTDIR=${ZDOTDIR:-$HOME} \
+    ZSH=$ZSH \
+    ${DISPLAY+DISPLAY=$DISPLAY} \
+    ${SSH_TTY+SSH_TTY=$SSH_TTY} \
+    ${SSH_AUTH_SOCK+SSH_AUTH_SOCK=$SSH_AUTH_SOCK} \
+    ${ZSH_NAME} -i -l
 
 (( $+commands[nix] )) && nixpkgs() {
     cmd=$1
