981d720c222beb6b273159c49184639eaac379cf
Vincent Bernat <vincent@bernat.ch>
2023-02-15 23:14:28 +0100

    history: simplify HISTFILE definition
    I don't have exceptions anymore (and I don't remember what they are)

--- rc/history.zsh
+++ rc/history.zsh
@@ -1,26 +1,11 @@
 # -*- sh -*-
 
-setopt extended_history	        # save timestamps
-setopt share_history            # share history accross zsh sessions
+setopt extended_history		# save timestamps
+setopt share_history			# share history accross zsh sessions
 setopt hist_ignore_all_dups	# ignores duplicates
-setopt hist_ignore_space        # don't store commands starting with a space
+setopt hist_ignore_space		# don't store commands starting with a space
 
-() {
-    local domain=${(pj:.:)${(ps:.:)HOSTNAME}[-2,-1]}
-    local hashed=${"$(print Ahyeigh4yo $domain | ${commands[sha256sum]:-$commands[sha256]} 2> /dev/null)"[1,16]}
-    case $hashed in
-        49e78cdf67755528)
-            typeset -g HISTFILE=~/.zsh_history
-            if [[ -O $ZSHRUN/history ]]; then
-                fc -R $ZSHRUN/history
-                fc -A
-                rm -f $ZSHRUN/history
-            fi
-            ;;
-        12e640a29535f352) typeset -g HISTFILE=~/.zhistory ;;
-        *) typeset -g HISTFILE=$ZSHRUN/history ;;
-    esac
-}
+HISTFILE=$ZSHRUN/history
 HISTSIZE=30000
 SAVEHIST=30000
 
