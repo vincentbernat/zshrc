7dc8e2bc432b82f1491ddbde0cea516b35c85ca5
Vincent Bernat <vincent@bernat.ch>
2023-07-26 22:07:30 +0200

    completion: make enter not accept directly the line

--- rc/05-completion.zsh
+++ rc/05-completion.zsh
@@ -92,8 +92,7 @@ bindkey -M menuselect "+" accept-and-menu-complete
 # Use fzf when available
 if (( $+commands[fzf] )) && [[ -f $ZSH/third-party/fzf-tab/fzf-tab.plugin.zsh ]]; then
     source $ZSH/third-party/fzf-tab/fzf-tab.plugin.zsh
-    zstyle ':fzf-tab:*' fzf-bindings 'space:accept' '+:toggle'
-    zstyle ':fzf-tab:*' accept-line enter
+    zstyle ':fzf-tab:*' fzf-bindings '+:toggle'
     zstyle ':fzf-tab:*' switch-group alt-left alt-right
     zstyle ':completion:*:descriptions' format ${PRCH[completion]}' %d'
 
