01e2b02cf9358422265f956903404b6f148f27a4
Vincent Bernat <vincent@bernat.ch>
2023-12-05 10:29:00 +0100

    tmux: use a Perl script to filter escape codes
    ansifilter does not strip backspace and stuff like that. The solution is
    still imperfect.

--- rc/tmux.zsh
+++ rc/tmux.zsh
@@ -46,8 +46,18 @@
     >> $out
 
     # Reformat
-    if (( $+commands[ansifilter] )); then
-        ansifilter -x 256G $out | gzip -c > ${out%.rawlog}.log.gz
+    if (( $+commands[perl] )); then
+        perl -e 'while (<>) {
+  s/ \e[ #%()*+\-.\/]. |
+    \r | # Remove extra carriage returns also
+    (?:\e\[|\x9b) [ -?]* [@-~] | # CSI ... Cmd
+    (?:\e\]|\x9d) .*? (?:\e\\|[\a\x9c]) | # OSC ... (ST|BEL)
+    (?:\e[P^_]|[\x90\x9e\x9f]) .*? (?:\e\\|\x9c) | # (DCS|PM|APC) ... ST
+    \e.|[\x80-\x9f] //xg;
+    1 while s/[^\b][\b]//g;  # remove all non-backspace followed by backspace
+  print;
+}' \
+            < $out | gzip -c > ${out%.rawlog}.log.gz
     else
         local current_limit=$(tmux show-options -gv history-limit)
         tmux set -g history-limit 2147483647
