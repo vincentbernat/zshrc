6ad80b4209af656a54a22dbf98415830047c92d5
Vincent Bernat <vincent@bernat.ch>
2024-10-06 18:36:48 +0200

    build: don't try to provide a NixOS module
    This would need some adaptation for system-wide installation.

--- flake.nix
+++ flake.nix
@@ -7,15 +7,7 @@
         ".zshrc".source = "${self}/zshrc";
       } // lib.mapAttrs'
         (n: v: lib.nameValuePair ".zsh/${builtins.baseNameOf n}" { source = "${self}/${n}"; })
-        (lib.filterAttrs (n: v: v == "directory") (builtins.readDir self));
-    };
-    nixosModules.default = { lib, ... }: {
-      environment.etc = {
-        "zshenv".source = lib.mkForce "${self}/zshenv";
-        "zshrc".source = lib.mkForce "${self}/zshrc";
-      } // lib.mapAttrs'
-        (n: v: lib.nameValuePair "zsh/${builtins.baseNameOf n}" { source = "${self}/${n}"; })
-        (lib.filterAttrs (n: v: v == "directory") (builtins.readDir self));
+        (lib.filterAttrs (n: v: v == "directory" && n != "run") (builtins.readDir self));
     };
   };
 }
